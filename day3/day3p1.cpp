5471c225aff870c8e6f0a9962a947c9e69aa8fc4
Zach Poole <httpsZach@icloud.com>
2023-12-23 01:48:25 -0800

    update comments

--- day3/day3p1.cpp
+++ day3/day3p1.cpp
@@ -13,6 +13,7 @@ int countLines(std::ifstream &file, std::string fileName) {
 	return numLines;
 }
 
+// takes in the number of lines, and appends each line to a new vector of strings that it then returns
 std::vector<std::string> createArray(int numLines, std::ifstream &file, std::string fileName) {
 	std::vector<std::string> vec;
 	std::string _;
@@ -33,11 +34,13 @@ int main(int argc, char** argv) {
 	std::cin >> fileName;
 	std::ifstream myFile;
 
+	// open file, get number of lines, and then close the file
 	myFile.open(fileName);
 	int numLines = countLines(myFile, fileName);
 	std::cout << numLines << std::endl;
 	myFile.close();
 
+	// open file, create vector of strings of each line, then close the file
 	myFile.open(fileName);
 	std::vector<std::string> arr = createArray(numLines, myFile, fileName);
 	myFile.close();
@@ -46,7 +49,6 @@ int main(int argc, char** argv) {
 		std::cout << arr[i] << std::endl;
 	}
 
-	myFile.close();
 	return 1;
 	
 }
